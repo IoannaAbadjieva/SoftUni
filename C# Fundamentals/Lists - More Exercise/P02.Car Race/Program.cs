cf6549be031278c9d430bcc6c9ead4d75d7b2b6b
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:43:16 +0200

    Added Lists  More Exercise

---
+++ C# Fundamentals/Lists - More Exercise/P02.Car Race/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace P02.Car_Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] racersTimes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            double leftRacerTime = GetLeftRacerTime(racersTimes);
+            double rightRacerTime = GetRightRacerTime(racersTimes);
+
+            if (leftRacerTime < rightRacerTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftRacerTime}");
+            }
+            else if (leftRacerTime > rightRacerTime)
+            {
+                Console.WriteLine($"The winner is right with total time: {rightRacerTime}");
+            }
+        }
+
+        static double GetLeftRacerTime(int[] racersTimes)
+        {
+            double leftSum = 0;
+
+            for (int index = 0; index < racersTimes.Length / 2; index++)
+            {
+                int currStepTime = racersTimes[index];
+                leftSum += currStepTime;
+
+                if (currStepTime == 0)
+                {
+                    leftSum -= leftSum * 0.2;
+                }
+            }
+            return leftSum;
+        }
+        static double GetRightRacerTime(int[] racersTimes)
+        {
+            double rightSum = 0;
+
+            for (int index = racersTimes.Length - 1; index > racersTimes.Length / 2; index--)
+            {
+                int currStepTime = racersTimes[index];
+                rightSum += currStepTime;
+
+                if (currStepTime == 0)
+                {
+                    rightSum -= rightSum * 0.20;
+                }
+            }
+
+            return rightSum;
+        }
+    }
+}
---
+++ C# Fundamentals/Lists - More Exercise/P03.Take Skip Rope/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace P03.Take_Skip_Rope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> inputLine = Console.ReadLine().ToCharArray().ToList();
+            List<int> numbers = new List<int>();
+            List<int> takeList = new List<int>();
+            List<int> skipList = new List<int>();
+
+            RemoveDigits(inputLine, numbers);
+            DivideNumbersList(numbers, takeList, skipList);
+            Console.WriteLine(DecryptMessage(inputLine, takeList, skipList));
+
+        }
+
+        static void RemoveDigits(List<char> inputLine, List<int> numbers)
+        {
+            for (int index = 0; index < inputLine.Count; index++)
+            {
+                char currCh = inputLine[index];
+
+                if (char.IsDigit(currCh))
+                {
+                    numbers.Add(currCh - 48);
+                    inputLine.RemoveAt(index);
+                    index--;
+                }
+            }
+        }
+
+        static void DivideNumbersList(List<int> numbers, List<int> takeList, List<int> skipList)
+        {
+            for (int index = 0; index < numbers.Count; index++)
+            {
+                int currNumber = numbers[index];
+
+                if (index % 2 != 0)
+                {
+                    skipList.Add(currNumber);
+                }
+                else
+                {
+                    takeList.Add(currNumber);
+                }
+            }
+        }
+        static string DecryptMessage(List<char> inputLine, List<int> takeList, List<int> skipList)
+        {
+            string input = new string(inputLine.ToArray());
+            StringBuilder result = new StringBuilder();
+            char[] currSequense;
+            int skipped = 0;
+
+            for (int index = 0; index < skipList.Count; index++)
+            {
+                currSequense = input
+                    .Skip(skipped)
+                    .Take(takeList[index])
+                    .ToArray();
+
+                result.Append(new string(currSequense));
+                skipped += takeList[index];
+                skipped += skipList[index];
+            }
+
+            return result.ToString();
+        }
+    }
+}

3769bb32dd8ab010ed9fade51d069b15b6cd34ab
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:45:26 +0200

    Added Methods  More Exercise

---
+++ C# Fundamentals/Methods - More Exercise/P01.Data Types/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace P01.Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "int")
+            {
+                int input = int.Parse(Console.ReadLine());
+                PrintResult(input);
+            }
+            else if (type == "real")
+            {
+                double input = double.Parse(Console.ReadLine());
+                PrintResult(input);
+            }
+            else if (type == "string")
+            {
+                string input = Console.ReadLine();
+                PrintResult(input);
+            }
+        }
+
+        static void PrintResult(int input)
+        {
+            Console.WriteLine(2 * input);
+        }
+
+        static void PrintResult(double input)
+        {
+            Console.WriteLine($"{1.5 * input:f2}");
+        }
+
+        static void PrintResult(string input)
+        {
+            Console.WriteLine($"${input}$");
+        }
+
+    }
+}
---
+++ C# Fundamentals/Methods - More Exercise/P05.Multiplication Sign/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P05.Multiplication_Sign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+            double thirdNum = double.Parse(Console.ReadLine());
+
+            PrintMultiplicationSign(firstNum, secondNum, thirdNum);
+        }
+
+        static void PrintMultiplicationSign(double num1, double num2, double num3)
+        {
+            bool condition1 = num1 < 0 && num2 < 0 && num3 > 0;
+            bool condition2 = num1 < 0 && num2 > 0 && num3 < 0;
+            bool condition3 = num1 > 0 && num2 < 0 && num3 < 0;
+
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if (condition1 || condition2 || condition3)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (num1 < 0 || num2 < 0 || num3 < 0)
+            {
+                Console.WriteLine("negative");
+            }
+
+            else
+            {
+                Console.WriteLine("positive");
+            }
+        }
+    }
+}

4c4f4348d9df870a8fd988952fe1603f3ac5e89c
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 08:58:31 +0200

    Added  Streams, Files and Directories  Homeworks

---
+++ CSharp Advanced/Streams, Files and Directories - Exercises/DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,66 @@
+﻿namespace DirectoryTraversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+
+            SortedDictionary<string, List<FileInfo>> extensions = new SortedDictionary<string, List<FileInfo>>();
+
+            string[] files = Directory.GetFiles(inputFolderPath);
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                if (!extensions.ContainsKey(fileInfo.Extension))
+                {
+                    extensions.Add(fileInfo.Extension, new List<FileInfo>());
+                }
+                extensions[fileInfo.Extension].Add(fileInfo);
+            }
+
+            Dictionary<string, List<FileInfo>> orderedExtensions = extensions
+                .OrderByDescending(x => x.Value.Count())
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var extension in orderedExtensions)
+            {
+                sb.AppendLine(extension.Key);
+
+                List<FileInfo> orderedFiles = extension.Value
+                    .OrderByDescending(x => x.Length)
+                    .ToList();
+
+                foreach (var file in orderedFiles)
+                {
+                    sb.AppendLine($"--{file.Name} - {(double)file.Length / 1024:f3}kb");
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName, textContent);
+        }
+    }
+}
---
+++ CSharp Advanced/Streams, Files and Directories - Lab/WordCount/WordCount.cs
@@ -0,0 +1,43 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            string[] words = File.ReadAllText(wordsFilePath).ToLower().Split();
+            string text = File.ReadAllText(textFilePath).ToLower();
+
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+
+            foreach (string word in words)
+            {
+                string pattern = $"\\b{word}\\b";
+                MatchCollection matches = Regex.Matches(text, pattern);
+                wordCounts[word] = matches.Count();
+            }
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var kvp in wordCounts.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{kvp.Key} - {kvp.Value}");
+                }
+            }
+        }
+    }
+}

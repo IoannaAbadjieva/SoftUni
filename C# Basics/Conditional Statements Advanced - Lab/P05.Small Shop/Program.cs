bdb3b2750dbd804f884f4b955841efb6d93d7711
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:23:05 +0200

    Added Conditional Statements Advanced  Lab

---
+++ C# Basics/Conditional Statements Advanced - Lab/P05.Small Shop/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace P05.Small_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string sity = Console.ReadLine();
+            double quontity = double.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (sity == "Sofia")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        price = 0.50;
+                        break;
+
+                    case "water":
+                        price = 0.80;
+                        break;
+
+                    case "beer":
+                        price = 1.20;
+                        break;
+
+                    case "sweets":
+                        price = 1.45;
+                        break;
+
+                    case "peanuts":
+                        price = 1.60;
+                        break;
+                }
+            }
+
+            else if (sity == "Plovdiv")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        price = 0.40;
+                        break;
+
+                    case "water":
+                        price = 0.70;
+                        break;
+
+                    case "beer":
+                        price = 1.15;
+                        break;
+
+                    case "sweets":
+                        price = 1.30;
+                        break;
+
+                    case "peanuts":
+                        price = 1.50;
+                        break;
+                }
+            }
+            else if (sity == "Varna")
+            {
+                switch (product)
+
+                {
+                    case "coffee":
+                        price = 0.45;
+                        break;
+
+                    case "water":
+                        price = 0.70;
+                        break;
+
+                    case "beer":
+                        price = 1.10;
+                        break;
+
+                    case "sweets":
+                        price = 1.35;
+                        break;
+
+                    case "peanuts":
+                        price = 1.550;
+                        break;
+                }
+            }
+            double totalPrice = quontity * price;
+
+            if (totalPrice > 0)
+            {
+                Console.WriteLine(totalPrice);
+            }
+
+        }
+    }
+}
---
+++ C# Basics/Conditional Statements Advanced - Lab/P12.Trade Commissions/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace P12.Trade_Commissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+
+            double percent = 0;
+
+            if (sales > 0 && sales <= 500)
+            {
+                if (city == "Sofia")
+                {
+                    percent = 0.05;
+                }
+                else if (city == "Varna")
+                {
+                    percent = 0.045;
+                }
+                else if (city == "Plovdiv")
+                {
+                    percent = 0.055;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (sales > 500 && sales <= 1000)
+            {
+                if (city == "Sofia")
+                {
+                    percent = 0.07;
+                }
+                else if (city == "Varna")
+                {
+                    percent = 0.075;
+                }
+                else if (city == "Plovdiv")
+                {
+                    percent = 0.08;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (sales > 1000 && sales <= 10000)
+            {
+                if (city == "Sofia")
+                {
+                    percent = 0.08;
+                }
+                else if (city == "Varna")
+                {
+                    percent = 0.1;
+                }
+                else if (city == "Plovdiv")
+                {
+                    percent = 0.12;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (sales > 10000)
+            {
+                if (city == "Sofia")
+                {
+                    percent = 0.12;
+                }
+                else if (city == "Varna")
+                {
+                    percent = 0.13;
+                }
+                else if (city == "Plovdiv")
+                {
+                    percent = 0.145;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+            double commission = sales * percent;
+            if (commission > 0)
+            {
+                Console.WriteLine("{0:f2}", commission);
+            }
+
+        }
+    }
+}

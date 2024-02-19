3177b31d78c8d5274367d17cc6b11b9173c53b93
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 11:00:09 +0200

    Added Sets and Dictionaries Advanced  Lab

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/P03.Largest 3 Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P03.Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+              .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToArray();
+
+            int[] sortedNumbers = numbers.OrderByDescending(x => x).Take(3).ToArray();
+            Console.WriteLine(String.Join(' ', sortedNumbers));
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/P04.Product Shop/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P04.Product_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Dictionary<string, double>> shops = new SortedDictionary<string, Dictionary<string, double>>();
+
+            while (true)
+            {
+                string product = Console.ReadLine();
+                if (product == "Revision")
+                {
+                    break;
+                }
+
+                string[] productInfo = product.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string shopName = productInfo[0];
+                string productName = productInfo[1];
+                double price = double.Parse(productInfo[2]);
+
+                if (!shops.ContainsKey(shopName))
+                {
+                    shops[shopName] = new Dictionary<string, double>();
+                }
+                shops[shopName][productName] = price;
+            }
+
+            foreach (var shop in shops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}

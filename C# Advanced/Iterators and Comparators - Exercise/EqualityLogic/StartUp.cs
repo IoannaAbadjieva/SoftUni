6d42eb5cfc40a0b5ba52bc7a666557085b9c7f36
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:57:36 +0200

    Added Iterators and Comparators  Exercise

---
+++ C# Advanced/Iterators and Comparators - Exercise/EqualityLogic/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Person> hashset = new HashSet<Person>();
+            SortedSet<Person> sortedset = new SortedSet<Person>();
+
+            int count =int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Person newPerson = new Person(personInfo[0], int.Parse(personInfo[1]));
+
+                hashset.Add(newPerson);
+                sortedset.Add(newPerson);
+            }
+
+            Console.WriteLine(hashset.Count);
+            Console.WriteLine(sortedset.Count);
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/Stack/Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private const int InitialCapacity = 4;
+        private T[] internalArray;
+
+        public Stack()
+        {
+            internalArray = new T[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            if (Count == internalArray.Length)
+            {
+                Resize();
+            }
+            internalArray[Count++] = item;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            T itemToPop = internalArray[Count - 1];
+            internalArray[Count - 1] = default;
+            Count--;
+
+            return itemToPop;
+
+        }
+
+        private void Resize()
+        {
+            T[] newArray = internalArray;
+
+            for (int i = 0; i < Count; i++)
+            {
+                newArray[i] = internalArray[i];
+            }
+
+            internalArray = newArray;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return internalArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

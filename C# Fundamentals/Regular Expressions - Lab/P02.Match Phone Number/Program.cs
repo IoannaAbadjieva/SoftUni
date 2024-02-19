624b3f85746643f8763a25a79b4fdad016710b81
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:48:23 +0200

    Added Regular Expressions  Lab

---
+++ C# Fundamentals/Regular Expressions - Lab/P02.Match Phone Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace P02.Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359( |-)[2]\1[0-9]{3}\1[0-9]{4}\b";
+
+            MatchCollection matches = Regex.Matches(Console.ReadLine(), pattern);
+
+            string[] phones = matches
+                .Cast<Match>()
+                .Select(x => x.Value)
+                .ToArray();
+
+            Console.WriteLine(String.Join(", ", phones));
+        }
+    }
+}
---
+++ C# Fundamentals/Regular Expressions - Lab/P03.Match Dates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace P03.Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})([\.\-\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            MatchCollection matches = Regex.Matches(Console.ReadLine(), pattern);
+
+            foreach (Match date in matches)
+            {
+                string day = date.Groups["day"].Value;
+                string month = date.Groups["month"].Value;
+                string year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}

93e301f1a78da1441796a73c3871bc7a22546387
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:37:32 +0200

    Added Basic Syntax  More Exercise

---
+++ C# Fundamentals/Basic Syntax - More Exercise/P03.Gaming Store/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace P03.Gaming_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = double.Parse(Console.ReadLine());
+            
+            double price = 0.0;
+            double money = balance;
+            string game = Console.ReadLine();
+            while (game != "Game Time")
+            {
+                if (game == "OutFall 4" || game == "RoverWatch Origins Edition") price = 39.99;
+                else if (game == "CS: OG") price = 15.99;
+                else if (game == "Zplinter Zell") price = 19.99;
+                else if (game == "Honored 2") price = 59.99;
+                else if (game == "RoverWatch") price = 29.99;
+                else
+                {
+                    Console.WriteLine("Not Found");
+                    game = Console.ReadLine();
+                    continue;
+                }
+
+                if (balance >= price)
+                {
+                    balance -= price;
+                    Console.WriteLine($"Bought {game}");
+                    if (balance == 0)
+                    {
+                        Console.WriteLine("Out of money!");
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Too Expensive");
+                }
+                game = Console.ReadLine();
+            }
+            Console.WriteLine($"Total spent: ${money - balance:f2}. Remaining: ${balance:f2}");
+        }
+    }
+}
---
+++ C# Fundamentals/Basic Syntax - More Exercise/P05.Messages/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace P05.Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string message = string.Empty;
+            int offset;
+            int main;
+
+            while (input != string.Empty)
+            {
+                main = int.Parse(input[0].ToString());
+
+                if (main == 0)
+                {
+                    message += (char)32;
+                    input = Console.ReadLine();
+                    continue;
+                }
+                if (main == 8 || main == 9)
+                {
+                    offset = (main - 2) * 3 + 1;
+                }
+                else
+                {
+                    offset = (main - 2) * 3;
+                }
+                message += (char)(offset + input.Length - 1 + 97);
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(message);
+
+        }
+    }
+}

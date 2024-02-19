d4529f17e216a8f8cb46ff56660aa2fff44deca3
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:59:01 +0200

    Added Multidimensional Arrays  Lab

---
+++ C# Advanced/Multidimensional Arrays - Lab/P03.Primary Diagonal/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P03.Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string[] rowElements = Console.ReadLine().Split(' ');
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = int.Parse(rowElements[col]);
+                }
+            }
+
+            int sum = 0;
+            for (int row = 0; row < n; row++)
+            {
+                sum += matrix[row, row];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

4665d28c003d45ed11dd1367116f44cdbff19bb6
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:35:47 +0200

    Added Associative Arrays  Lab

---
+++ C# Fundamentals/Associative Arrays - Lab/P02.Odd Occurrences/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P02.Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> occurencies = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string wordToLower = word.ToLower();
+
+                if (occurencies.ContainsKey(wordToLower))
+                {
+                    occurencies[wordToLower]++;
+                }
+                else
+                {
+                    occurencies.Add(wordToLower, 1);
+                }
+            }
+
+            string[] result = occurencies
+                .Where(x => x.Value % 2 != 0)
+                .Select(x => x.Key)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}

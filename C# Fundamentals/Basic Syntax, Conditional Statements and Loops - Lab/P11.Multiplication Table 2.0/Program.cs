f570f4da4c17985448b191f876ef85825b065999
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:38:21 +0200

    Added Basic Syntax Lab

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/P11.Multiplication Table 2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P11.Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            if (multiplier > 10)
+            {
+                Console.WriteLine($"{number} X {multiplier} = {number * multiplier}");
+            }
+            else
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    Console.WriteLine($"{number} X {i} = {number * i}");
+                }
+            }
+        }
+    }
+}

49e2d90b117f17a13dacdc71eaa58bb1141be555
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:40:34 +0200

    Added Data Types and Variables  More Exercise

---
+++ C# Fundamentals/Data Types and Variables - More Exercise/P01.Data Type Finder/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P01.Data_Type_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (int.TryParse(input, out int intValue))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (double.TryParse(input, out double doubleValue))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input, out char charValue))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (bool.TryParse(input, out bool boolValue))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+            }
+        }
+    }
+}

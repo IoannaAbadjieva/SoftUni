1e7c102bd683b453f0e9b8ebdef1aa57fbc7e025
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:34:33 +0200

    Added Arrays More Exercise

---
+++ C# Fundamentals/Arrays - More Exercise/P03.Recursive Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P03.Recursive_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            numbers = new long[n + 1];
+
+            Console.WriteLine(Fibonacci(n));
+        }
+
+        static long[] numbers;
+
+        static long Fibonacci(int n)
+        {
+            if (numbers[n] != 0)
+            {
+                return numbers[n];
+            }
+
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            long result = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+            numbers[n] = result;
+
+            return result;
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays - More Exercise/P05.Longest Increasing Subsequence/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace P05.Longest_Increasing_Subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            int maxLenght = 0;
+            int maxLastIndex = 0;
+
+            int[] len = new int[nums.Length];
+            int[] prev = new int[nums.Length];
+
+            for (int currNumIndex = 0; currNumIndex < nums.Length; currNumIndex++)
+            {
+                len[currNumIndex] = 1;
+                prev[currNumIndex] = -1;
+
+                for (int prevNumIndex = 0; prevNumIndex < currNumIndex; prevNumIndex++)
+                {
+                    if (nums[currNumIndex] > nums[prevNumIndex] && len[currNumIndex] < len[prevNumIndex] + 1)
+                    {
+                        len[currNumIndex] = len[prevNumIndex] + 1;
+                        prev[currNumIndex] = prevNumIndex;
+                    }
+                }
+                if (len[currNumIndex] > maxLenght)
+                {
+                    maxLenght = len[currNumIndex];
+                    maxLastIndex = currNumIndex;
+                }
+            }
+          
+            int[] LIS = new int[maxLenght];
+            int prevIndex = maxLastIndex;
+
+            for (int i = 0; i < LIS.Length; i++)
+            {
+                LIS[i] = nums[prevIndex];
+                prevIndex = prev[prevIndex];
+            }
+
+            Array.Reverse(LIS);
+            Console.WriteLine(string.Join(" ", LIS));
+
+
+        }
+    }
+}

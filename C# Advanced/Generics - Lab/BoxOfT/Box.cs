2443750725ccdc4bd2814522f40d3d668df3847a
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:57:08 +0200

    Added Generics  Lab

---
+++ C# Advanced/Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public int Count { get { return list.Count; } }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public T Remove()
+        {
+            T elementToRemove = list.Last();
+            list.RemoveAt(list.Count -1);
+            return elementToRemove;
+        }
+
+    }
+}
---
+++ C# Advanced/Generics - Lab/GenericScale/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<string> equalityScale = new EqualityScale<string>("left","Left");
+            Console.WriteLine(equalityScale.AreEqual());
+        }
+    }
+}

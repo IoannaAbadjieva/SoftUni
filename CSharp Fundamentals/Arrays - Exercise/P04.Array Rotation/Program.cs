ed91819a39e2971e3c98b39fa8b809a97b22fd9e
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 06:37:37 +0200

    Added Arrays Exercise

---
+++ CSharp Fundamentals/Arrays - Exercise/P04.Array Rotation/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P04.Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                   .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                   .Select(int.Parse)
+                   .ToArray();
+
+            int rotations = int.Parse(Console.ReadLine());
+            int rotationsCount = rotations % numbers.Length;
+
+            for (int count = 1; count <= rotationsCount; count++)
+            {
+                int firstElementValue = numbers[0];
+
+                for (int index = 0; index < numbers.Length - 1; index++)
+                {
+                    numbers[index] = numbers[index + 1];
+                }
+
+                numbers[numbers.Length - 1] = firstElementValue;
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
---
+++ CSharp Fundamentals/Arrays - Exercise/P10.LadyBugs/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace P10.LadyBugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+
+            int[] ladybugsIndexes = Console.ReadLine()
+                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                     .Select(int.Parse)
+                     .ToArray();
+
+            int[] field = new int[fieldSize];
+
+            for (int index = 0; index < fieldSize; index++)
+            {
+                if (ladybugsIndexes.Contains(index))
+                {
+                    field[index] = 1;
+                }
+            }
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                int ladybugIndex = int.Parse(cmdArgs[0]);
+                string direction = cmdArgs[1];
+                int flyLenght = int.Parse(cmdArgs[2]);
+
+                if (ladybugIndex < 0 || ladybugIndex >= fieldSize)
+                {
+                    continue;
+                }
+
+                if (field[ladybugIndex] == 0)
+                {
+                    continue;
+                }
+                int landingIndex = ladybugIndex;
+
+                while (true)
+                {
+                    field[ladybugIndex] = 0;
+
+                    if (direction == "left")
+                    {
+                        landingIndex -= flyLenght;
+                    }
+                    else if (direction == "right")
+                    {
+                        landingIndex += flyLenght;
+                    }
+
+                    if (landingIndex < 0 || landingIndex >= fieldSize)
+                    {
+                        break;
+                    }
+                    if (field[landingIndex] == 0)
+                    {
+                        break;
+                    }
+                }
+
+
+                if (landingIndex >= 0 && landingIndex < fieldSize)
+                {
+                    field[landingIndex] = 1;
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', field));
+        }
+    }
+}

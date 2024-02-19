5dba07d1fe448148b86ef04cfb6c4a4eaf193934
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:47:04 +0200

    Added Objects and Classes  More Exercise

---
+++ C# Fundamentals/Objects and Classes - More Exercise/P04.Raw Data/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace P04.Raw_Data
+{
+    class Engine
+    {
+        public Engine(int speed, int power)
+        {
+            Speed = speed;
+            Power = power;
+        }
+
+        public int Speed { get; set; }
+
+        public int Power { get; set; }
+    }
+
+    class Cargo
+    {
+        public Cargo(int weight, string type)
+        {
+            Weight = weight;
+            Type = type;
+        }
+
+        public int Weight { get; set; }
+
+        public string Type { get; set; }
+    }
+
+    class Car
+    {
+        public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType)
+        {
+            Model = model;
+            Engine = new Engine(engineSpeed, enginePower);
+            Cargo = new Cargo(cargoWeight, cargoType);
+        }
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Cargo Cargo { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] carArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carArgs[0];
+                int engineSpeed = int.Parse(carArgs[1]);
+                int enginePower = int.Parse(carArgs[2]);
+                int cargoWeight = int.Parse(carArgs[3]);
+                string cargoType = carArgs[4];
+
+                Car newCar = new Car(model, engineSpeed, enginePower, cargoWeight, cargoType);
+                cars.Add(newCar);
+            }
+
+            string command = Console.ReadLine();
+            List<Car> filteredCars = FilterCars(cars, command);
+
+            filteredCars.ForEach(x => Console.WriteLine(x.Model));
+        }
+
+        static List<Car> FilterCars(List<Car> cars, string filter)
+        {
+            List<Car> filteredCars = new List<Car>();
+
+            if (filter == "fragile")
+            {
+                filteredCars = cars
+                   .Where(x => x.Cargo.Type == filter && x.Cargo.Weight < 1000)
+                   .ToList();
+            }
+            else if (filter == "flamable")
+            {
+                filteredCars = cars
+                    .Where(x => x.Cargo.Type == filter && x.Engine.Power > 250)
+                    .ToList();
+            }
+
+            return filteredCars;
+        }
+    }
+}
---
+++ C# Fundamentals/Objects and Classes - More Exercise/P05.Shopping Spree/Program.cs
@@ -0,0 +1,146 @@
+﻿using System.Text;
+
+namespace P05.Shopping_Spree
+{
+    class Person
+    {
+        public Person(string name, int money)
+        {
+            Name = name;
+            Money = money;
+            BagOfProducts = new List<Product>();
+        }
+
+        public string Name { get; set; }
+
+        public int Money { get; set; }
+
+        public List<Product> BagOfProducts { get; set; }
+
+        public void BuyProduct(Product product)
+        {
+            if (Money >= product.Cost)
+            {
+                Money -= product.Cost;
+                BagOfProducts.Add(product);
+                Console.WriteLine($"{Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} can't afford {product.Name}");
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{Name} - ");
+
+            if (BagOfProducts.Count == 0)
+            {
+                sb.Append("Nothing bought");
+            }
+            else
+            {
+
+                for (int i = 0; i < BagOfProducts.Count; i++)
+                {
+                    if (i == BagOfProducts.Count - 1)
+                    {
+                        sb.Append(BagOfProducts[i].Name);
+                    }
+                    else
+                    {
+                        sb.Append($"{BagOfProducts[i].Name}, ");
+                    }
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+    class Product
+    {
+        public Product(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public string Name { get; set; }
+
+        public int Cost { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> persons = new List<Person>();
+            List<Product> products = new List<Product>();
+
+
+            string[] personsArgs = Console.ReadLine()
+                .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int index = 0; index < personsArgs.Length; index++)
+            {
+                string[] currPersonArgs = personsArgs[index]
+                    .Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                string personName = currPersonArgs[0];
+                int personMoney = int.Parse(currPersonArgs[1]);
+
+                Person newPerson = new Person(personName, personMoney);
+                persons.Add(newPerson);
+            }
+
+            string[] productsArgs = Console.ReadLine()
+               .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int index = 0; index < productsArgs.Length; index++)
+            {
+                string[] currProductArgs = productsArgs[index]
+                    .Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                string productName = currProductArgs[0];
+                int productCost = int.Parse(currProductArgs[1]);
+
+                Product newProduct = new Product(productName, productCost);
+                products.Add(newProduct);
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string personName = cmdArgs[0];
+                string productName = cmdArgs[1];
+
+                Person searchedPerson = persons
+                    .FirstOrDefault(x => x.Name == personName);
+
+                if (searchedPerson == null)
+                {
+                    continue;
+                }
+
+                Product searchedProduct = products
+                    .FirstOrDefault(x => x.Name == productName);
+                if (searchedProduct == null)
+                {
+                    continue;
+                }
+
+                searchedPerson.BuyProduct(searchedProduct);
+            }
+
+            persons.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}

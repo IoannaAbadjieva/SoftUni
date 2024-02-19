a0aa9ff2a62d4b76a874d22e1acde934859e81f6
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-02-19 10:46:33 +0200

    Added Objects and Classes  Lab

---
+++ C# Fundamentals/Objects and Classes - Lab/P03.Songs/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace P03.Songs
+{
+    class Song
+    {
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+
+        public Song(string typeList, string name, string time)
+        {
+            this.TypeList = typeList;
+            this.Name = name;
+            this.Time = time;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSongs = int.Parse(Console.ReadLine());
+
+            List<Song> songs = new List<Song>();
+
+            for (int i = 1; i <= numberOfSongs; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split("_", StringSplitOptions.RemoveEmptyEntries);
+
+                string typeList = input[0];
+                string name = input[1];
+                string time = input[2];
+
+                Song currentSong = new Song(typeList, name, time);
+                songs.Add(currentSong);
+            }
+
+            string filter = Console.ReadLine();
+
+            if (filter == "all")
+            {
+                foreach (var song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                List<Song> filteredSongs = songs.FindAll(x => x.TypeList == filter);
+
+                foreach (var song in filteredSongs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Objects and Classes - Lab/P05.Students 2.0/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace P05.Students_2._0
+{
+    class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Hometown = homeTown;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Hometown { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] studentInfo = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                int age = int.Parse(studentInfo[2]);
+                string hometown = studentInfo[3];
+
+                if (IsStudentExisting(students, firstName, lastName))
+                {
+                    Student student = students.Find(x => x.FirstName == firstName && x.LastName == lastName);
+                    student.Age = age;
+                    student.Hometown = hometown;
+                    continue;
+                }
+
+                Student currentStudent = new Student(firstName, lastName, age, hometown);
+                students.Add(currentStudent);
+            }
+
+            string city = Console.ReadLine();
+            PrintFilteredStudentsList(students, city);
+        }
+
+        static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
+        {
+            foreach (var student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static void PrintFilteredStudentsList(List<Student> students, string city)
+        {
+            List<Student> filteredList = students.FindAll(x => x.Hometown == city);
+
+            foreach (var student in filteredList)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+}
